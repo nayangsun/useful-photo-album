68863551783cee5ad06a0905bc0af76a655fada8
N <nyangsun@gmail.com>
2021-07-29 11:35:52 +0900

    210729 paging debug

--- app/src/main/java/com/example/useful_photo_album/data/UnsplashPagingSource.kt
+++ app/src/main/java/com/example/useful_photo_album/data/UnsplashPagingSource.kt
@@ -1,6 +1,7 @@
 package com.example.useful_photo_album.data
 
 import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.example.useful_photo_album.api.UnsplashService
 import com.example.useful_photo_album.data.remote.UnsplashPhoto
 
@@ -10,7 +11,7 @@ class UnsplashPagingSource(
     private val service: UnsplashService,
     private val type: UnsplashRepository.QueryType
 ) : PagingSource<Int, UnsplashPhoto>() {
-    
+
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, UnsplashPhoto> {
         val page = params.key ?: UNSPLASH_STARTING_PAGE_INDEX
         return try {
@@ -31,7 +32,7 @@ class UnsplashPagingSource(
     }
 
     /**
-     * sealed class를 어떻게 이용할지 고민하다, 결국 만들어진 sealed를 이용해 data class 할당을 해서
+    * sealed class를 어떻게 이용할지 고민하다, 결국 만들어진 sealed를 이용해 data class 할당을 해서
      * 이렇게 바꿔봤는데요, load 부분에서 조금더 직관적으로 보이지 않나라는 생각을 해봅니다.
      * 그래도 sealed class로 바꿔보고 싶은데요. 혹시 간단한 힌트라도 알려주신다면 감사하겠습니다.
      */
@@ -51,6 +52,10 @@ class UnsplashPagingSource(
                 QueryResult(response.results, prevKey, nextKey)
             }
         }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, UnsplashPhoto>): Int? {
+        TODO("Not yet implemented")
     }
 
     /*
