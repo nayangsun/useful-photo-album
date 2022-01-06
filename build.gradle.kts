7751c63aed87f1e569ed4aa82c280f1f3262d27d
Malgeon <nyangsun@gmail.com>
2022-01-06 14:15:21 +0900

    add - spotless 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@
 
 // Top-level build file where you can add configuration options common to all
 // sub-projects/modules.
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
@@ -33,6 +34,11 @@ buildscript {
     }
 }
 
+plugins {
+    id("com.diffplug.gradle.spotless") version "3.27.1"
+}
+
+
 allprojects {
     repositories {
         google()
@@ -43,3 +49,44 @@ allprojects {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+
+subprojects {
+    apply(plugin = "com.diffplug.gradle.spotless")
+    val ktlintVer = "0.40.0"
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            ktlint(ktlintVer).userData(
+                mapOf("max_line_length" to "100", "disabled_rules" to "import-ordering")
+            )
+            licenseHeaderFile(project.rootProject.file("copyright.kt"))
+        }
+        kotlinGradle {
+            // same as kotlin, but for .gradle.kts files (defaults to '*.gradle.kts')
+            target("**/*.gradle.kts")
+            ktlint(ktlintVer)
+            licenseHeaderFile(project.rootProject.file("copyright.kt"), "(plugins |import |include)")
+        }
+    }
+
+    // `spotlessCheck` runs when a build includes `check`, notably during presubmit. In these cases
+    // we prefer `spotlessCheck` run as early as possible since it fails in seconds. This prevents a
+    // build from running for several minutes doing other intensive tasks (resource processing, code
+    // generation, compilation, etc) only to fail on a formatting nit.
+    // Using `mustRunAfter` avoids creating a task dependency. The order is enforced only if
+    // `spotlessCheck` is already scheduled to run, so we can still build and launch from the IDE
+    // while the code is "dirty".
+    tasks.whenTaskAdded {
+        if (name == "preBuild") {
+            mustRunAfter("spotlessCheck")
+        }
+    }
+
+    // TODO: Remove when the Coroutine and Flow APIs leave experimental/internal/preview.
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions.freeCompilerArgs +=
+            "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
+    }
+}
+
